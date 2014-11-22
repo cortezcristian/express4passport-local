e577bd4b0feabe363f9d01c882aa9faed7370ee3
Cristian Cortez <cortez.cristian@gmail.com>
2014-11-21 21:07:13 -0300

    Initial setup

---
+++ routes/main.js
@@ -0,0 +1,64 @@
+var app = module.parent.exports.app;
+var Persons = require('../models/persons.js');
+
+app.get('/list', function(req, res){
+    var msg = req.flash('message');
+    Persons.find({}, function(err, docs){
+        res.render('list', { title: 'List', persons: docs, flashmsg: msg});
+    });
+});
+
+app.get('/p/new', function(req, res){
+    req.flash('message', 'You visited /new');
+    res.render('new', { title: 'New'});
+});
+
+app.post('/p/new', function(req, res){
+    console.log(req.body);
+    var p = new Persons({ name: req.body.name, age: req.body.age });
+    p.save(function(err, doc){
+        if(!err){
+            res.redirect('/list');
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.get('/p/delete/:id', function(req, res){
+    Persons.remove({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            res.redirect('/list');
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.get('/p/edit/:id', function(req, res){
+    Persons.findOne({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            res.render('edit', { title: 'Edit', person: doc});
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.post('/p/edit/:id', function(req, res){
+    Persons.findOne({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            doc.name = req.body.name; 
+            doc.age = req.body.age;
+            doc.save(function(err, doc){
+                if(!err){
+                    res.redirect('/list');
+                } else {
+                    res.end(err);    
+                }    
+            }); 
+        } else {
+            res.end(err);    
+        }    
+    });
+});
